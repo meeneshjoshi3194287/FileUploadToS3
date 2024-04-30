0b4dd1e6267e6bde0af788b92d00ff1c8ecee2d7
Meenesh Joshi <manujoshi221@gmail.com>
2024-04-30 06:59:47 +0530

    removed keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import AWS from "aws-sdk";
+import { useState } from "react";
 
 function App() {
+  // Create state to store file
+  const [file, setFile] = useState(null);
+
+  // Function to upload file to s3
+  const uploadFile = async () => {
+    // S3 Bucket Name
+    const S3_BUCKET = "s3bucket-assignment-01";
+
+    // S3 Region
+    const REGION = "ap-south-1";
+
+    const s3 = new AWS.S3({
+      params: { Bucket: S3_BUCKET },
+      region: REGION,
+    });
+
+    // Files Parameters
+
+    const params = {
+      Bucket: S3_BUCKET,
+      Key: file.name,
+      Body: file,
+    };
+
+    // Uploading file to s3
+
+    var upload = s3
+      .putObject(params)
+      .on("httpUploadProgress", (evt) => {
+        // File uploading progress
+        console.log(
+          "Uploading " + parseInt((evt.loaded * 100) / evt.total) + "%"
+        );
+      })
+      .promise();
+
+    await upload.then((err, data) => {
+      console.log(err);
+      // Fille successfully uploaded
+      alert("File uploaded successfully.");
+    });
+  };
+  // Function to handle file and store it to file state
+  const handleFileChange = (e) => {
+    // Uploaded file
+    const file = e.target.files[0];
+    // Changing file state
+    setFile(file);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload a File to Amazon S3</h1>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={uploadFile}>Upload</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import S3Uploader from './S3Uploader';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <S3Uploader />
+    <App />
   </React.StrictMode>
 );
 
